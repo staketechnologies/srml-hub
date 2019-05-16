31f584cb0939613ea7608f9d47ccde99999d8bcc
Task Ohmori <tascii.oh@gmail.com>
2019-05-16 11:40:51 +0900

    modal done

--- src/App.js
+++ src/App.js
@@ -1,74 +1,104 @@
 import React from 'react';
 import logo from './logo.svg';
-import {Card, Col, Row, Container} from 'react-bootstrap'
-import Modal from 'react-modal'
+import {
+  Card,
+  Col,
+  Row,
+  CardDeck,
+  Container,
+  Modal,
+  Button
+} from 'react-bootstrap'
 import './App.css';
 
-
-
-function Readme(){
-  return(
-    {__html : "<p>readme here</p>"
-    })
+function Readme() {
+  return (<p>readme here</p>)
 }
 
-function OneCard() {
-  return (<Card  elementType = <a/> style = {{marginBottom: '30px'}}>
-    <Card.Body>
-      <Card.Title><h1>Plasm</h1></Card.Title>
-      <iframe  src="http://ghbtns.com/github-btn.html?user=stakedtechnologies&repo=Plasm&type=watch&count=true" allowtransparency="true" frameborder="0" scrolling="0" width="100" height="20"></iframe>
-      <span></span>
-      <hr/>
-      <Card.Text>
-      description here
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>);
-}
+var models = [
+  {
+    name: "Plasm",
+    user: "stakedtechnologies",
+    repo: "Plasm",
+    descripstion: "Allows to add Plasma functions to their Substrate chain easily and seamlessly. Developers can also make both plasma parent chains and plasma child chains with Substrate."
+  }, {
+    name: "Substrate POA",
+    user: "gautamdhameja",
+    repo: "substrate-poa",
+    descripstion: "Allows addition of new validators in a pure PoA(Proof of Authority) fashion. No tokens or stake needed, just the authorities have to agree."
+  },
+]
 
-Modal.setAppElement('#root')
-
-function App() {
-  var cards = [];
-  for (var i = 0; i < 100; i++) {
-    cards.push(<Col sm={6} md = {4} lg = {3}><OneCard/></Col>);
-  }
-  return (<div className="App">
-    <Container fluid={true}>
-      <Row className ="row-eq-height">
-        {cards}
-      </Row>
-    </Container>
-  </div>);
-}
-
-const customStyles = {
-    overlay: {
-      position: 'fixed',
-      top: 0,
-      left: 0,
-      right: 0,
-      bottom: 0,
-      backgroundColor: 'rgba(255, 255, 255, 0.75)'
-    },
-    content: {
-      position: 'absolute',
-      top: '40px',
-      left: '40px',
-      right: '40px',
-      bottom: '40px',
-      border: '1px solid #ccc',
-      background: '#fff',
-      overflow: 'auto',
-      WebkitOverflowScrolling: 'touch',
-      borderRadius: '4px',
-      outline: 'none',
-      padding: '20px'
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      model: {},
+      modalIsOpen: false
     }
-  };
+    this.afterOpenModal = this.afterOpenModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+  }
+  openModal(model) {
+    this.setState({model: model, modalIsOpen: true});
+  }
 
+  closeModal() {
+    this.setState({modalIsOpen: false});
+  }
+
+  afterOpenModal() {}
+
+  renderModelPage(model) {
+    return (<div>
+      <Modal.Header closeButton="closeButton">
+        <Modal.Title id="contained-modal-title-vcenter">
+          {model.name}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        readme here
+      </Modal.Body>
+      <Modal.Footer></Modal.Footer>
+    </div>);
+  }
+
+  OneCard(props) {
+    return (<div className="rounded border p-2" style={{
+        marginBottom: '30px',
+        cursor: 'pointer'
+      }} onClick={this.openModal.bind(this, props.model)}>
+      <div className="m-1">
+        <h2>{props.model.name}</h2>
+      </div>
+      <iframe src={"https://ghbtns.com/github-btn.html?user=" + props.model.user + "&repo=" + props.model.repo + "&type=star&count=true"} frameborder="0" scrolling="0" width="110px" height="20px"></iframe>
+      <hr/>
+      <div className="m-1">
+        {props.model.descripstion}
+      </div>
+    </div>);
+  }
+
+  render() {
+    var cards = [];
+    for (var m of models) {
+      cards.push(<Col xs={6} lg={4} xl={3} className="d-flex align-items-stretch">{this.OneCard({model: m})}</Col>);
+    }
+    return (<div className="App">
+      <Container fluid={false}>
+        <Row style={rowEqHeight}>
+          {cards}
+        </Row>
+      </Container>
+      <Modal show={this.state.modalIsOpen} onHide={this.closeModal} size="lg" aria-labelledby="contained-modal-title-vcenter" centered="centered">
+        {this.renderModelPage(this.state.model)}
+      </Modal>
+    </div>);
+  }
+}
+const rowEqHeight = {
+  display: "flex",
+  flexFlow: "row wrap"
+}
 
 export default App;
