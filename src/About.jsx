661c16d0e066d54e478eeed74e36c152391370cb
satellitex <s.a.t.e.3.ths@gmail.com>
2019-08-07 01:22:02 +0900

    fix about messages

--- src/About.jsx
+++ src/About.jsx
@@ -20,10 +20,10 @@ class About extends React.Component {
                 <h1>About</h1>
                 </Jumbotron>
                 <hr />
-                    <p>SubstrateHub(SRMLHub) curates and provides a platform for Substrate blockchain developer to easily find SubstrateRuntimeModuleLibraies or SubsrateChains for a variety of platforms and uses. We regularly update the site and provide filtering functionality for users to find libraries that they need, either for educational purposes, imports your chains, or other uses.</p>
-                    <p>Like us on follow us on <a href="https://twitter.com/staketech">Twitter</a>!</p>
-                    <p>Do you have a suggestion for Substrate Library, or any comments about the site? Drop us an email at info[at]staked.co.jp!</p>
-                    <p>SRMLHub was built with love in <a href = "https://stake.co.jp">StakeTechnologies</a>.</p>
+                    <p align='left'> SubstrateHub(SRMLHub) curates and provides a platform for Substrate blockchain developer to easily find SubstrateRuntimeModuleLibraies or SubsrateChains for a variety of platforms and uses. We regularly update the site and provide filtering functionality for users to find libraries that they need, either for educational purposes, imports your chains, or other uses.</p>
+                    <p align='left'> Like us on follow us on <a href="https://twitter.com/staketech">Twitter</a>! or please issue on <a href="https://github.com/stakedtechnologies/srml-hub">GitHub Repository</a>!</p>
+                    <p align='left'> Do you have a suggestion for Substrate Library, or any comments about the site? Drop us an email at info[at]staked.co.jp!</p>
+                    <p align='left'> SRMLHub was built with love in <a href = "https://stake.co.jp">StakeTechnologies</a>.</p>
             </Container>
         </div>);
       }
