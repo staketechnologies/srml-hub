0c89382334d036d8e004d0c25e9d6fa6a1f53dd6
satellitex <s.a.t.e.3.ths@gmail.com>
2019-08-28 15:27:28 +0900

    add react-addoc and add models

--- src/models.js
+++ src/models.js
@@ -1,5 +1,17 @@
 var models = [
   {
+    name: "Substrate",
+    user: "paritytech",
+    repo: "substrate",
+    descripstion: "Substrate is a next-generation framework for blockchain innovation."
+  },
+  {
+    name: "Polkadot",
+    user: "paritytech",
+    repo: "polkadot",
+    descripstion: "In 2017 we split our implementation of \"Polkadot\" from its platform-level component \"Substrate\". When we split them, we split the Polkadot code off into another repo (this repo), leaving the Substrate repo to be what used to be Polkadot, along with its branches and releases."
+  },
+  {
     name: "Plasm",
     user: "stakedtechnologies",
     repo: "Plasm",
@@ -57,6 +69,54 @@ var models = [
     user: "evolutionlandorg",
     repo: "darwinia-appchain",
     descripstion: "Application chain in Darwinia network based on substrate framework."
+  },
+  {
+    name: "Joystream",
+    user: "Joystream",
+    repo: "substrate-node-joystream",
+    descripstion: "Joystream node built on top of Substrate. Follow the instructions below to download the software or build it from source. Further instructions for windows and mac can be found here. Linux should be similar to mac."
+  },
+  {
+    name: "Substrate multisig",
+    user: "mixbytes",
+    repo: "substrate-module-multisig",
+    descripstion: "Experimental Substrate module for multisignature wallet management. DO NOT use it to store any funds or as a base for production code. At the moment it's just a playground."
+  },
+  {
+    name: "Merkle tree module",
+    user: "filiplazovic",
+    repo: "substrate-merkle-tree",
+    descripstion: "A new SRML-based Substrate node, ready for hacking."
+  },
+  {
+    name: "Parking Space Registry",
+    user: "yjkimjunior",
+    repo: "ParkingSpaceSubstrate",
+    descripstion: "Parking Space Registry, silly example of using Substrate Node Template to launch a chain. You rent a space by adding a balance to it, you can't rent spaces with a balance remaining on it."
+  },
+  {
+    name: "Substrate Inherents Sample",
+    user: "substrate-developer-hub",
+    repo: "substrate-inherents-sample",
+    descripstion: "A Substrate runtime sample showing how to use inherent data in your custom modules. In this sample, we get some data from a WebAPI and store it on-chain using Substrate inherents."
+  },
+  {
+    name: "Simple Collateral",
+    user: "nczhu",
+    repo: "collateral",
+    descripstion: "A simple implementation of tokenized debt, with simple-interest. A token owner can collateralize a non-fungible token for a loan, and pay it back with interest. Creditors can seize the collateral upon loan default."
+  },
+  {
+    name: "CidChain Substrate Node",
+    user: "Polygos",
+    repo: "substrate-node-cidchain",
+    descripstion: "CidChain node implementation, made with Substrate."
+  },
+  {
+    name: "Katal",
+    user: "Trinkler",
+    repo: "katal-chain",
+    descripstion: "Katal: Standard Framework for Finance. View on Telemetry."
   },
 ]
 
